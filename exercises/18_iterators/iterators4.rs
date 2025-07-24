148975a12eb2f5abd0dc7505d389dcc116944c78
chaihuibin <77320373+chaihuibin926@users.noreply.github.com>
2025-07-24 14:56:34 +0800

    iterators

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -9,7 +9,11 @@ fn factorial(num: u64) -> u64 {
     // - imperative style loops (for/while)
     // - additional variables
     // For an extra challenge, don't use:
-    // - recursion
+    // -    
+
+    // 等效
+    // (1..=num).fold(1, |acc, x| acc * x)
+    (1..=num).product()
 }
 
 fn main() {
